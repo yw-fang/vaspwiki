e9b36489d5e33b334910a7722fbf8a5ccd1eaa44
yw-fang <fyuewen@gmail.com>
2021-08-26 11:56:40 +0900

    update to 1.1.2 version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def readme():
 
 
 setup(name='vaspwiki',
-	  version='1.1.1',
+	  version='1.1.2',
 	  description='The VASP manual',
 	  long_description=long_description,
 	  long_description_content_type='text/x-rst',
--- vaspwiki/version.py
+++ vaspwiki/version.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python3
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 __author__ = 'Yue-Wen FANG'
 __maintainer__ = "Yue-Wen FANG"
 __email__ = 'fyuewen@gmail.com'
 __license__ = 'MIT License'
-__release_data__= 'Dec. 27, 2018'
+__release_date__= 'Dec. 27, 2018'
+__revision_date__= 'Dec. 27, 2018'
