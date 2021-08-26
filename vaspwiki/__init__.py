3831ba8ffaf699bb2b0995d2db073b2a5f72c9fd
yw-fang <fyuewen@gmail.com>
2021-08-26 11:57:44 +0900

    fix

--- vaspwiki/__init__.py
+++ vaspwiki/__init__.py
@@ -2,7 +2,7 @@
 from vaspwiki.version import __version__
 from vaspwiki.version import __author__
 from vaspwiki.version import __maintainer__
-from vaspwiki.version import __release_data__
+from vaspwiki.version import __release_date__
 from vaspwiki.version import __license__
 
 __all__ = ['vaspwiki']
